a7665823f4ee8aeb64de1d8021488585bb88f2df
youdaoqiu <youdaoqiu@126.com>
2018-01-12 18:53:40 +0800

    UI
    新增UI模块

---
+++ workspace/Android/Code/INet_LIB/inetlib/src/main/java/com/yandaoqiu/net/ui/IImageLoader.java
@@ -0,0 +1,20 @@
+package com.yandaoqiu.net.ui;
+
+import android.view.View;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by yandaoqiu on 2018/1/12.
+ */
+
+public class IImageLoader {
+
+public IImageLoader(){
+
+}
+
+
+
+
+}
